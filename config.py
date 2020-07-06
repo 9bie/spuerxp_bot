3fbd489e9f13e34c9fa277cc8f54dda8d888ad56
9bie <blackguwc@163.com>
2020-07-06 14:33:20 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# Base
+DOWNLOAD_PATH = "/data/"
+
+
+# Telegram
+TELEGRAM_BOT_TOKEN = ""
+IS_WEBHOOKING = False
+WEBHOOK_URL = ""
+
+# E-hentai
+EX_COOKIES = ""
---
+++ manage.py
@@ -0,0 +1,4 @@
+from flask import *
+app = Flask(__name__)
+if __name__ == '__main__':
+    app.run()
