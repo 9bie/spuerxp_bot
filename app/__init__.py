20f7e6ced31c6684cbe8597e7b57c5e9d7dd584a
TerraceCN <zky3213@gmail.com>
2020-07-08 21:25:50 +0800

    docker

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from telebot import apihelper
 import telebot
 import config
 
-apihelper.proxy = {"https": "socks5://127.0.0.1:10808"}
+# apihelper.proxy = {"https": "socks5://127.0.0.1:10808"}
 apihelper.ENABLE_MIDDLEWARE = True
 bot = telebot.TeleBot(config.TG_BOT_TOKEN)
 
