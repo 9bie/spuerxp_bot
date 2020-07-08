92a721835e2df137f963205df7d14ff071a17dff
TerraceCN <zky3213@gmail.com>
2020-07-08 09:59:26 +0800

    修改配置文件

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
-# Base
-DOWNLOAD_PATH = "/data/"
+import os
 
+# Base
+DOWNLOAD_PATH = os.environ.get("DOWNLOAD_PATH", "./data/")
 
 # Telegram
-TELEGRAM_BOT_TOKEN = ""
-IS_WEBHOOKING = False
-WEBHOOK_URL = ""
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "<THIS IS YOUR BOT TOKEN>")
 
 # E-hentai
-EX_COOKIES = ""
+EX_COOKIES = os.environ.get("EX_COOKIES", "<THIS IS YOUR EXHENTAI COOKIES>")
+
+# Telegra.ph
+PH_TOKEN = os.environ.get("PH_TOKEN", "")
